237b3f25f61104e891193e3b5df8fbb8e6ddacfe
janrawa <jrawa@ippt.pan.pl>
2024-08-19 10:49:18 +0200

    Swaped from WorkerThread to WorkerProcess

--- worker_thread.py
+++ worker_thread.py
@@ -2,23 +2,6 @@ import multiprocessing
 import threading
 import time
 
-class WorkerThread(threading.Thread):
-    def __init__(self, target_function, *args, **kwargs):
-        super().__init__()
-        self.target_function = target_function
-        self.args = args
-        self.kwargs = kwargs
-        self.stop_event = threading.Event()
-
-    def run(self):
-        while not self.stop_event.is_set():
-            # Execute the target function with arguments
-            self.target_function(*self.args, **self.kwargs)
-
-    def stop(self):
-        self.stop_event.set()
-
-
 class WorkerProcess(multiprocessing.Process):
     def __init__(self, target_function, *args, **kwargs):
         super().__init__()
