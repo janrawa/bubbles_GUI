1d37b2fa00be95343228a82d6f1e3cec8a7d2335
janrawa <jrawa@ippt.pan.pl>
2024-09-02 11:29:30 +0200

    Added typing

--- workers.py
+++ workers.py
@@ -1,3 +1,4 @@
+from typing import Callable
 import multiprocess
 import queue
 
@@ -6,7 +7,7 @@ class WorkerProcess(multiprocess.Process):
     Constantly running processes, that start and finish on rare ocations ex.
     begining data acquisition from an oscilloscope.
     """
-    def __init__(self, target_function, *args, **kwargs):
+    def __init__(self, target_function : Callable, *args, **kwargs):
         super().__init__()
         self.target_function = target_function
         self.args = args
@@ -26,7 +27,7 @@ class ConsumerProcess(multiprocess.Process):
     Short lasting processes that are run on rare ocations ex.
     saving gathered data to an archive file.
     """
-    def __init__(self, start):
+    def __init__(self, start:bool):
         super().__init__()
         self.process_queue = multiprocess.Queue()
         self.stop_event = multiprocess.Event()
@@ -46,7 +47,7 @@ class ConsumerProcess(multiprocess.Process):
             if target_function is not None:
                 target_function(*args, **kwargs)
     
-    def schedule_process(self, target, *args, **kwargs):
+    def schedule_process(self, target : Callable, *args, **kwargs):
         self.process_queue.put((target, args, kwargs))
 
     def stop(self):
