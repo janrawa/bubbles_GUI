ea38f85f614327f14c8793a7effaed7d4a6bad8c
janrawa <jrawa@ippt.pan.pl>
2024-09-19 11:48:56 +0200

    Changed device_type() return and added formatting function known_device_list()

--- known_devices.py
+++ known_devices.py
@@ -1,6 +1,7 @@
 # set of known oscilloscope idVendor and idProduct
 knownOscilloscopes = {
     ('0x957', '0x900d'), 
+    ('0x957', '0x900b'),
 }
 # set of known generators idVendor and idProduct
 knownGenerators = {
@@ -11,8 +12,20 @@ def device_type(device) -> str:
     ids = (hex(device.idVendor), hex(device.idProduct))
 
     if ids in knownOscilloscopes:
-        return 'Oscilloscope'
+        return 'Osc'
     if ids in knownGenerators:
-        return 'Generator'
+        return 'Gen'
     
-    return 'Uknown device'
+    return 'UknDev'
+
+def known_device_list() -> tuple:
+    from usbtmc import list_devices
+    devices=list_devices()
+
+    return (
+        devices,
+        [
+            f'{hex(dev.idVendor)}:{hex(dev.idProduct)} {device_type(dev)}' \
+            for dev in devices
+        ]
+    )
