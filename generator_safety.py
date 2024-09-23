9635d9eb462cacb105159b2b8809a6007cdc1062
janrawa <jrawa@ippt.pan.pl>
2024-09-23 16:11:08 +0200

    Added docstring. Changed numba return parameter

--- generator_safety.py
+++ generator_safety.py
@@ -4,7 +4,7 @@ from numpy.typing import ArrayLike
 from scipy.fft import fftfreq, fft
 from scipy.signal import savgol_filter  # For advanced smoothing
 
-from numba import jit, float64
+from numba import jit, float64, boolean
 
 @jit(float64(float64, float64, float64))
 def clip(x:float, vmin:float, vmax:float) -> float:
@@ -12,6 +12,18 @@ def clip(x:float, vmin:float, vmax:float) -> float:
 
 @jit(float64(float64[:], float64[:], float64))
 def close_far_field_integral_fraction(xf: ArrayLike, yf_mag: ArrayLike, f: float):
+    """Calculates integral of Fourier magnitude (abs(fft(y))) of close and far field.
+    Close field is defined as +- 5% around peak frequency,
+    far field is defined as +- 10% around peak frequency.
+
+    Args:
+        xf (ArrayLike): frequency spectrum
+        yf_mag (ArrayLike): Fourier magnitude
+        f (float): peak frequency
+
+    Returns:
+        float: two times fraction of integrals
+    """
     f_close_arglim = (
         searchsorted(xf, f * 0.95),  # 95% of f
         searchsorted(xf, f * 1.05)   # 105% of f
@@ -31,25 +43,43 @@ def close_far_field_integral_fraction(xf: ArrayLike, yf_mag: ArrayLike, f: float
     # multiplied by 2 becouse close range is 10% and far range is 20%
     return 2*f_close_sum / f_far_sum
 
-@jit(float64(float64[:], float64[:], float64))
+@jit(boolean(float64[:], float64[:], float64))
 def subharmonics_present(xf: ArrayLike, yf_mag: ArrayLike, f0: float):
+    """Subharmonics detection function based on close_far_field_integral_fraction method.
+    Subharmonics detection values (found in return) are fitted using experimental data.
+
+    Args:
+        xf (ArrayLike): frequency spectrum
+        yf_mag (ArrayLike): Fourier magnitude
+        f0 (float): generator frequency
+
+    Returns:
+        bool: are subharmonics 3/2f0 OR 5/2f0 present?
+    """
     # yf_mag = savgol_filter(yf_mag, window_length=51, polyorder=2)
 
     peak32=close_far_field_integral_fraction(xf, yf_mag, 3/2 * f0)
     peak52=close_far_field_integral_fraction(xf, yf_mag, 5/2 * f0)
-    
-    # return {
-    #     '3/2f0':(peak32, peak32>0.95), # detection threshold set by experimentation
-    #     '5/2f0':(peak52, peak52>1.0),  # detection threshold set by experimentation
-    # }
 
     return (peak32>0.95) or (peak52>1.0)
 
 @jit(float64(float64, float64[:], float64[:], float64))
-def calculate_voltage(v0 : float, xf: ArrayLike, yf: ArrayLike, f0: float) -> float:
+def calculate_voltage(v0 : float, xf: ArrayLike, yf_mag: ArrayLike, f0: float) -> float:
+    """Calculates new voltage value based on the current voltage and presence of subharmonics.
+    Maximum rate of change is dv = 0.02 V. Resoulting voltage is cliped to stay below 2V for savety reasons.
+
+    Args:
+        v0 (float): current voltage
+        xf (ArrayLike): frequency spectrum
+        yf_mag (ArrayLike): Fourier magnitude
+        f0 (float): generator frequency
+
+    Returns:
+        float: new voltage
+    """
     dv = 0.02 # voltage change
 
-    subharmonics=subharmonics_present(xf, yf, f0)
+    subharmonics=subharmonics_present(xf, yf_mag, f0)
 
     # subharmonics present -> drop voltage
     if subharmonics == True:
@@ -61,6 +91,12 @@ def calculate_voltage(v0 : float, xf: ArrayLike, yf: ArrayLike, f0: float) -> fl
 
 class RollingRegister(list):
     def __init__(self, maxlen : int) -> None:
+        """Rolling register based on list. Keeps last maxlen records.
+        Used for calculating rolling averedges of past recorded values.
+
+        Args:
+            maxlen (int): maximum number of values/objects keps in register
+        """
         self._maxlen = maxlen
 
     @property
@@ -74,6 +110,11 @@ class RollingRegister(list):
 
 class AmplitudeRegulator:
     def __init__(self, window_length : int) -> None:
+        """Regulates generator voltage
+
+        Args:
+            window_length (int): number of signal samples - used for averaging of signal
+        """
         self.voltageRegister   = RollingRegister(window_length)
     
     def updateAmplitude(self, sample_rate : float,
